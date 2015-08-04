0952e81bfabcdd450f04823bd33ac6a311057a3a
Daniel Mihalca <danimihalca@gmail.com>
2015-08-04 17:26:38 +0300

    Added online/offline icon for contact...

--- app/src/main/java/dm/chatclient/utils/ContactListAdapter.java
+++ app/src/main/java/dm/chatclient/utils/ContactListAdapter.java
@@ -1,10 +1,14 @@
 package dm.chatclient.utils;
 
+import android.app.Application;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import dm.chatclient.model.Contact;
 
@@ -15,9 +19,15 @@ import dm.chatclient.R;
 
 public class ContactListAdapter extends ArrayAdapter<Contact>
 {
+    private Drawable m_onlineDrawable;
+    private Drawable m_offlineDrawable;
+
     public ContactListAdapter(Context context)
     {
         super(context, 0, new ArrayList<Contact>());
+
+        m_onlineDrawable = getContext().getResources().getDrawable(R.drawable.online);
+        m_offlineDrawable = getContext().getResources().getDrawable(R.drawable.offline);
     }
 
     public ContactListAdapter(Context context, List<Contact> contacts)
@@ -44,11 +54,20 @@ public class ContactListAdapter extends ArrayAdapter<Contact>
 
         TextView userNameView = (TextView) view.findViewById(R.id.userNameView);
         TextView fullNameView = (TextView) view.findViewById(R.id.fullNameView);
-
+        ImageView onlineImageView = (ImageView) view.findViewById(R.id.onlineImageView);
         Contact contact = getItem(i);
-        
+
         userNameView.setText(contact.getUserName());
         fullNameView.setText(contact.getFullName());
+
+        if (contact.isOnline())
+        {
+            onlineImageView.setImageDrawable(m_onlineDrawable);
+        }
+        else
+        {
+            onlineImageView.setImageDrawable(m_offlineDrawable);
+        }
 
         return view;
     }
